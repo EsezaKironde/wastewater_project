303d8e530d3abd44d169361fcfbd277903ff8c59
Zoli S. <zolismith19@gmail.com>
2025-04-18 10:54:17 -0400

    Final updates to summary statistics script

--- code/1_summary_statistics.R
+++ code/1_summary_statistics.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #set directory to project directory
 
 #Eseza's computer: setwd("C:/Users/nesez/OneDrive/Desktop/Data_Science_Toolkit/midterm/wastewater_project")
@@ -25,26 +24,7 @@ table_one <-raw_wastewater_data %>%
 
 saveRDS(table_one, here("output", "table_one.rds"))
 
-=======
 
-data_path <- here("data", "NWSS_Public_SARS-CoV-2_Wastewater_Metric_Data_20250307.csv")
-ww_data <- read_csv(data_path)
-
-table_one <- ww_data %>%
-  group_by(wwtp_jurisdiction) %>%
-  summarise(
-    n_counties = n_distinct(county_names),
-    n_fips = n_distinct(county_fips),
-    n_sewersheds = n_distinct(key_plot_id),
-    n_sample_locations = n_distinct(sample_location),
-    avg_population_served = round(mean(population_served, na.rm = TRUE), 0),
-    .groups = "drop"
-  ) %>%
-  arrange(desc(n_sewersheds))
-
-saveRDS(table_one, here("output", "table_one.rds"))
-
->>>>>>> upstream/main
 
 
 
